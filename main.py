21dd0318e73009954ea848dca840aae66de903d5
Alpha-1729 <anilakskumar1729@gmail.com>
2020-06-27 01:45:00 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+
+#!usr/bin/python3
+# Code to make a simple calculator(GUI) using python3
+
+from tkinter import Tk
+
+root=Tk()
+root.geometry("500x500")
+root.title("Simple Calculator")
+root.mainloop()
